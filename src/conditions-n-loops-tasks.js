9d8dfa22972c98cfdcb9490371fa755e43555800
Sapheeaa <sophia_lukyanchik@mail.ru>
2025-11-02 16:03:48 +0300

    feat: add two first tasks

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -21,8 +21,11 @@
  *  0  => true
  *  -5 => false
  */
-function isPositive(/* number */) {
-  throw new Error('Not implemented');
+function isPositive(number) {
+  if (number >= 0) {
+    return true;
+  }
+  return false;
 }
 
 /**
@@ -38,8 +41,20 @@ function isPositive(/* number */) {
  *  -5, 0, 5      => 5
  *  -0.1, 0, 0.2  => 0.2
  */
-function getMaxNumber(/* a, b, c */) {
-  throw new Error('Not implemented');
+function getMaxNumber(a, b, c) {
+  let maxNum = 0;
+  for (let i = 0; i < 4; i += 1) {
+    if (a >= maxNum) {
+      maxNum = a;
+    }
+    if (b >= maxNum) {
+      maxNum = b;
+    }
+    if (c >= maxNum) {
+      maxNum = c;
+    }
+  }
+  return maxNum;
 }
 
 /**
